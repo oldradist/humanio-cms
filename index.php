ccfdf92875d19004683ca6044277e51848c0c924
oldradist <ameerzon@humanio.net>
2016-03-18 13:50:55 +0100

    Added files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+
+include ("./config/globals.php");
+
+include("./includes/header.php");
+
+include("./includes/navigation.php");
+
+$query = $_SERVER["QUERY_STRING"];
+
+parse_str($query);
+
+$base = $site;	
+
+if ( strlen($base) == 0 ) { $base = "home"; }	
+
+$left = 'lblock';
+$right = 'rblock';
+$position = '';
+
+$verz = dir ( './texte' );
+
+//##################
+
+$files = scandir('./texte', $blog);
+
+echo "<div  class=placeholder>";
+include("texte/placeholder.txt");
+echo "</div>";
+
+
+foreach ($files as $entry)
+{
+
+	if ( (substr($entry, -3) === 'txt') && (substr($entry, 0, 4) === (substr($base, 0, 4))) )
+	{
+		if ( strlen($position) == 0 )
+		{
+			$position = $left;
+		}
+		elseif ( substr($position, 0, 1) == 'l' )
+		{
+			$position = $right;
+		}
+		elseif ( substr($position, 0, 1) == 'r' )
+		{
+			$position = $left;
+		}
+		else
+		{
+			$position = $left;
+		}
+		
+		echo "<div  class=$position>";
+		include("texte/$entry");
+		echo "</div>";
+	}
+
+} 
+
+
+
+$verz->close ();
+
+include("./includes/service.php");
+
+include("./includes/footer.php");
+?>
